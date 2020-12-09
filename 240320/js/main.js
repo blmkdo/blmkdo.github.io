2bdd94935587e5e067e9ae13b696fb9916beb079
Lazio Bold <74559830+blmkdo@users.noreply.github.com>
2020-12-09 17:44:00 +0500

    Add files via upload

---
+++ 240320/js/main.js
@@ -0,0 +1,28 @@
+var btn = document.getElementById('btn');
+btn.onclick = function () {
+	var text = document.querySelector('p.intro');
+	text.classList.add('green');
+}
+
+$(function () {
+	$(window).scroll(function() {
+	    $('#future .section-title').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.pluses').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInRight");
+	        }
+	    });
+	});
+})
